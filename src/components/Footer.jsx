987558749f32760602b14a89a593f36bea111ef1
Mario Bais <mariocfbais@gmail.com>
2022-06-10 19:27:33 -0300

    Corrigido destino no waze

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,8 @@ export default props =>
             </h6>
             <h6 className="fs-6 my-0 mx-1">Fone (62): 3225-3888 e-mail: sindifisco.go@terra.com.br</h6>
         </div>
-        <a target="_blank" className="btn btn-primary btn-sm d-flex" type="button" href="https://www.waze.com/pt-BR/live-map/directions/br/go/sindifisco-nacional-ds-goiania?to=place.ChIJw7ZRV5_xXpMR3VZFzIJEfTQ">
+        <a target="_blank" className="btn btn-primary btn-sm d-flex" type="button"
+            href="https://www.waze.com/pt-BR/live-map/directions?to=ll.-16.68885806%2C-49.23896313">
             <div className="my-auto"> Onde Encontrar</div></a>
         <div className="d-flex">
             <h6 className="mb-0">
