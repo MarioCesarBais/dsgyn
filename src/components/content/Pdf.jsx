5df7d4058ea32585c73bf6dee5b6971b45965ee4
Mario Bais <mariocfbais@gmail.com>
2022-06-11 17:02:25 -0300

    Documentos Funcionando. Deletados aquivos youtube

--- src/components/content/Pdf.jsx
+++
@@ -1,8 +0,0 @@
-export default () =>
-    <div>
-        <div>
-            <span>Page: <span id="page_num"></span> / <span id="page_count"></span></span>
-        </div>
-
-        <canvas id="the-canvas"></canvas>
-    </div>
