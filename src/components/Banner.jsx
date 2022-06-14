84eb9b3d25c018417a4578b10cc9b6497c8e6003
Mario Bais <mariocfbais@gmail.com>
2022-06-14 17:40:50 -0300

    Banner Ocupação Horizontal

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,8 +1,10 @@
 import dsgyn from '../imgs/sedepaisagem.jpg';
-import sedeDRF from "../imgs/protesto.jpg"
+import sedeDRF from "../imgs/protesto.jpg";
+import aeroporto from '../imgs/aeroporto.jpg'
 
 export default _ =>
-<div id='banner' className='d-flex'>
+<div id='banner' className='d-flex justify-content-evenly'>
     <img src={dsgyn} alt="frente sede DS Goiânia" />
     <img src={sedeDRF} alt="sede da DRF" />
+    <img src={aeroporto} alt="Aeroporto Goiânia" />
 </div>
--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,10 +1,13 @@
-import Logo from '../imgs/Logo.jpg'
-import brasao from '../imgs/brasao.png'
-import './logo.css'
+import { Link } from "react-router-dom";
+import Logo from "../imgs/Logo.jpg";
+import brasao from "../imgs/brasao.png";
+import "./logo.css";
 
-export default _ => (
-    <div id='logo-div'>
-        <img id='logo' src={Logo} alt='logotipo do Sindifisco Nacional' />
-        <img id='brasao' src={brasao} alt="pin" />
+export default () => (
+  <Link to="/">
+    <div id="logo-div">
+      <img id="logo" src={Logo} alt="logotipo do Sindifisco Nacional" />
+      <img id="brasao" src={brasao} alt="pin" />
     </div>
-)
+  </Link>
+);
